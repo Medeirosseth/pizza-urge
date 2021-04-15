86146faf5b54b24679d06968162a78ab36fb0155
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-15 08:15:52 -0700

    bugfix: remove global variable

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,23 @@ function Pizza(size, top1, top2, total){
   this.topping2 = top2;
   this.total = 0;
 }
-let newPizza = new Pizza("","","")
+
 
 Pizza.prototype.sizeCost=function(){
-  if (newPizza.size === "large"){
+  if (this.size === "large"){
     this.total = 15
   } else if (this.size === "medium"){
     this.total = 10
   } else if (this.size === "small")
     this.total = 5
-  if (newPizza.topping1 === "cheese"){
+  if (this.topping1 === "cheese"){
     this.total += 1
   } else if (this.topping1 === "mushroom"){
     this.total += 2
   } else if (this.topping1 === "pineapple"){
     this.total += 3
   } 
-  if (newPizza.topping2 === "black-olives"){
+  if (this.topping2 === "black-olives"){
     this.total += 1
   } else if (this.topping2 === "sun-dried-tomatoes"){
     this.total += 2
@@ -35,6 +35,7 @@ Pizza.prototype.sizeCost=function(){
 $(document).ready(function() {
   $("#pizzaForm").submit(function(event) {
     event.preventDefault();
+    let newPizza = new Pizza("","","")
     let size = $("select#size").val();
     newPizza.size = size  
     let toppingOne = $("select#topping1").val();
@@ -46,23 +47,7 @@ $(document).ready(function() {
     $("#top2").text(newPizza.topping2);
     $("#total").text(newPizza.sizeCost());
     $("#hiddenOrder").show();
-    let orderIn = new Pizza(size, toppingOne, toppingTwo)
+    
     
   });
 });
-
-
-// function moreOrders() {
-//   this.orders = []
-//   this.orderId = 0
-// }
-
-// moreOrders.prototype.addOrder=function(){
-//   order.id = this.assignId();
-//   this.orders.push(order);
-// }
-
-// moreOrders.prototype.assignId=function() {
-//   this.orderId += 1;
-//   return this.orderId;
-// }
